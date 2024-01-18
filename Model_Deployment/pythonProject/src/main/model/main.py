093a752a5bd20441f3c4662401725841688b67d5
Hristo-Karagyozov <H.Karagyozov@student.rug.nl>
2024-01-18 10:12:22 +0100

    fixed paths, partially trained model for the first time

--- Model_Deployment/pythonProject/src/main/model/main.py
+++ Model_Deployment/pythonProject/src/main/model/main.py
@@ -2,10 +2,10 @@ import evaluate
 import numpy as np
 import pandas as pd
 from datasets import Dataset
+from transformers import RobertaForSequenceClassification, Trainer, TrainingArguments, RobertaTokenizer
 from sklearn.model_selection import train_test_split
-from transformers import RobertaForSequenceClassification, Trainer, TrainingArguments
 
-from Model_Deployment.pythonProject.src.main.preprocessing import pipeline as preprocessing
+from src.main.preprocessing import pipeline as preprocessing
 
 
 def compute_metrics(eval_pred):
@@ -20,31 +20,41 @@ def compute_metrics(eval_pred):
 
 def load_tweets_and_labels():
     # read the tweets and emojis (labels) and put them in a pd df
-    with open("C:/Users/david/OneDrive/Desktop/University/Year 3/Machine Learning Practical/rug-ml-practical-group2/Model_Deployment/pythonProject/data/raw/test/us_test.text", "r", encoding="utf-8") as tweet_file:
-        tweets = tweet_file.readlines()
-
-    with open("C:/Users/david/OneDrive/Desktop/University/Year 3/Machine Learning "
-              "Practical/rug-ml-practical-group2/Model_Deployment/pythonProject/data/raw/test"
-              "/us_test.labels", "r", encoding="utf-8") as label_file:
+    with open("../../../data/raw/us_test.labels", "r", encoding="utf-8") as label_file:
         labels = label_file.readlines()
 
-    proper_df = [{'label': int(label), 'tweet': tweet} for label, tweet in zip(labels, tweets)]
+    with open("../../../data/raw/us_test.text", "r", encoding="utf-8") as tweet_file:
+        tweets = tweet_file.readlines()
 
-    return proper_df
+    formatted_df = [{'label': int(label), 'tweet': tweet} for label, tweet in zip(labels, tweets)]
+
+    return formatted_df
 
 
 def main():
+    tokenizer = RobertaTokenizer.from_pretrained("roberta-base")
+
     pipeline = preprocessing.Pipeline(load_tweets_and_labels())
     processed_data = pipeline.preprocess()
 
     # Convert the list of dictionaries to a Pandas DataFrame
     df = pd.DataFrame(processed_data)
 
-    # Create the dataset from the Pandas DataFrame
-    dataset = Dataset.from_pandas(df)
+    # Tokenize the tweet text and generate attention masks
+    tokenized_output = tokenizer(df['tweet'].tolist(), return_tensors="pt", padding=True, truncation=True,
+                                 max_length=64)
+    input_ids = tokenized_output["input_ids"]
+    attention_mask = tokenized_output["attention_mask"]
 
+    # Add input_ids and attention_mask to the DataFrame
+    df['input_ids'] = input_ids.tolist()
+    df['attention_mask'] = attention_mask.tolist()
 
-    train_dataset, test_dataset = train_test_split(dataset, train_size=0.8, random_state=42)
+    train_dataset, test_dataset = train_test_split(df, train_size=0.1, random_state=42)
+
+    # Convert the two datasets from pandas to dataset type again
+    train_dataset = Dataset.from_pandas(train_dataset)
+    test_dataset = Dataset.from_pandas(test_dataset)
 
     model = RobertaForSequenceClassification.from_pretrained("roberta-base", num_labels=20)
     training_args = TrainingArguments(output_dir="test_trainer")
